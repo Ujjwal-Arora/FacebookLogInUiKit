2ebabfcc6076565914b44eefbf78fe21836ef4d9
Ujjwal Arora <ukarora9@gmail.com>
2024-12-06 16:54:41 +0530

    Learnt Fb Login

--- FacebookLogInUiKit/SceneDelegate.swift
+++ FacebookLogInUiKit/SceneDelegate.swift
@@ -5,11 +5,26 @@
 //  Created by Ujjwal Arora on 06/12/24.
 //
 
+import FacebookCore
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else {
+            return
+        }
+
+        ApplicationDelegate.shared.application(
+            UIApplication.shared,
+            open: url,
+            sourceApplication: nil,
+            annotation: [UIApplication.OpenURLOptionsKey.annotation]
+        )
+    }
+
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
--- FacebookLogInUiKit/ViewController.swift
+++ FacebookLogInUiKit/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import FacebookLogin
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, LoginButtonDelegate {
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let token = AccessToken.current, !token.isExpired {
+            
+//            let token = token.tokenString
+//            let request = GraphRequest(graphPath: "me", parameters: ["fields" : "email , name"], tokenString: token, version: nil, httpMethod: .get)
+//            request.start { connection, result, error in
+//                print(result)
+//            }
+            logInFunction(token: token)
+            print("Already logged out")
+            
+        }else{
+            let loginButton = FBLoginButton()
+            loginButton.center = view.center
+            loginButton.delegate = self
+            loginButton.permissions = ["public_profile", "email"]
+            view.addSubview(loginButton)
+        }
     }
-
-
+    func loginButton(_ loginButton: FBSDKLoginKit.FBLoginButton, didCompleteWith result: FBSDKLoginKit.LoginManagerLoginResult?, error: (any Error)?) {
+        
+//        let token = result?.token?.tokenString
+//        let request = GraphRequest(graphPath: "me", parameters: ["fields" : "email , name"], tokenString: token, version: nil, httpMethod: .get)
+//        request.start { connection, result, error in
+//            print(result)
+//        }
+        logInFunction(token: result?.token)
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginKit.FBLoginButton) {
+        
+    }
+    func logInFunction(token : AccessToken?){
+        let token = token?.tokenString
+        let request = GraphRequest(graphPath: "me", parameters: ["fields" : "email , name"], tokenString: token, version: nil, httpMethod: .get)
+        request.start { connection, result, error in
+            print(result)
+        }
+    }
 }
-
